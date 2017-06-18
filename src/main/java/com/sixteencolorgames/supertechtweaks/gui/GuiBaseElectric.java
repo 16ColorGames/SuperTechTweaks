ae0db9eceb206f6d8bd6e0fc71e9b32c9f610f07
oa10712 <oa10712xbox@gmail.com>
2017-06-18 17:59:19 -0500

    1.2.2
    Added base files for use in other mods

---
+++ src/main/java/com/sixteencolorgames/supertechtweaks/gui/GuiBaseElectric.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sixteencolorgames.supertechtweaks.gui;
+
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.util.ResourceLocation;
+
+/**
+ *
+ * @author oa10712
+ */
+public abstract class GuiBaseElectric extends GuiBase {
+
+    public GuiBaseElectric(Container inventorySlotsIn, ResourceLocation background, InventoryPlayer playerInv, IInventory tile) {
+        super(inventorySlotsIn, background, playerInv, tile);
+    }
+
+    @Override
+    protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
+        super.drawGuiContainerBackgroundLayer(partialTicks, mouseX, mouseY);
+        int i = (this.width - this.xSize) / 2;
+        int j = (this.height - this.ySize) / 2;
+
+        int k = this.getEnergyLeftScaled(48);
+        this.drawTexturedModalRect(i + 10, j + 66 - k, 0, 30 - k + 206, 14, k);//render power
+    }
+
+    @Override
+    protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
+        super.drawGuiContainerForegroundLayer(mouseX, mouseY);
+        int i = (this.width - this.xSize) / 2;
+        int j = (this.height - this.ySize) / 2;
+        if (mouseX - i > 9 && mouseX - i < 24 && mouseY > j + 18 && mouseY < j + 66) {
+            String s = getEnergy() + "/" + getMaxEnergy();
+            this.fontRendererObj.drawString(s, mouseX - i, mouseY - j-10, 4210752);//Draw tile name
+        }
+    }
+
+    abstract int getEnergy();
+
+    abstract int getMaxEnergy();
+
+    abstract int getEnergyLeftScaled(int pixels);
+}
---
+++ src/main/java/com/sixteencolorgames/supertechtweaks/tileentities/TileEntityBase.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sixteencolorgames.supertechtweaks.tileentities;
+
+import javax.annotation.Nullable;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ISidedInventory;
+import net.minecraft.inventory.ItemStackHelper;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntityLockable;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ITickable;
+
+/**
+ *
+ * @author oa10712
+ */
+public abstract class TileEntityBase extends TileEntityLockable implements ITickable, ISidedInventory {
+
+    /**
+     * The ItemStacks that hold the items currently being used in the furnace
+     */
+    protected ItemStack[] itemStacks;
+    protected String customName;
+    private String name;
+
+    public TileEntityBase(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Returns the number of slots in the inventory.
+     */
+    public int getSizeInventory() {
+        return itemStacks.length;
+    }
+
+    /**
+     * Returns the stack in the given slot.
+     */
+    @Nullable
+    public ItemStack getStackInSlot(int index) {
+        return itemStacks[index];
+    }
+
+    /**
+     * Removes up to a specified number of items from an inventory slot and
+     * returns them in a new stack.
+     */
+    @Nullable
+    public ItemStack decrStackSize(int index, int count) {
+        return ItemStackHelper.getAndSplit(itemStacks, index, count);
+    }
+
+    /**
+     * Removes a stack from the given slot and returns it.
+     */
+    @Nullable
+    public ItemStack removeStackFromSlot(int index) {
+        return ItemStackHelper.getAndRemove(itemStacks, index);
+    }
+
+    /**
+     * Do not make give this method the name canInteractWith because it clashes
+     * with Container
+     */
+    public boolean isUseableByPlayer(EntityPlayer player) {
+        return this.worldObj.getTileEntity(this.pos) != this ? false
+                : player.getDistanceSq((double) this.pos.getX() + 0.5D, (double) this.pos.getY() + 0.5D,
+                        (double) this.pos.getZ() + 0.5D) <= 64.0D;
+    }
+
+    public void openInventory(EntityPlayer player) {
+    }
+
+    public void closeInventory(EntityPlayer player) {
+    }
+
+    /**
+     * Returns true if automation can insert the given item in the given slot
+     * from the given side.
+     */
+    public boolean canInsertItem(int index, ItemStack itemStackIn, EnumFacing direction) {
+        return this.isItemValidForSlot(index, itemStackIn);
+    }
+
+    public void clear() {
+        for (int i = 0; i < itemStacks.length; ++i) {
+            itemStacks[i] = null;
+        }
+    }
+
+    /**
+     * Get the name of this object. For players this returns their username
+     */
+    public String getName() {
+        return this.hasCustomName() ? this.customName : "container." + name;
+    }
+
+    /**
+     * Returns true if this thing is named
+     */
+    public boolean hasCustomName() {
+        return this.customName != null && !this.customName.isEmpty();
+    }
+
+    public void setCustomInventoryName(String p_145951_1_) {
+        this.customName = p_145951_1_;
+    }
+
+    /**
+     * Returns the maximum stack size for a inventory slot. Seems to always be
+     * 64, possibly will be extended.
+     */
+    public int getInventoryStackLimit() {
+        return 64;
+    }
+
+}
