2b12eb0541e6841fd3320d57c5b339d712e4238f
oa10712 <oa10712xbox@gmail.com>
2017-07-02 03:55:48 -0500

    1.3.2 version bump

--- src/main/java/com/sixteencolorgames/supertechtweaks/SuperTechTweaksMod.java
+++ src/main/java/com/sixteencolorgames/supertechtweaks/SuperTechTweaksMod.java
@@ -13,7 +13,7 @@ public class SuperTechTweaksMod {
 
     public static final String MODID = "supertechtweaks";
     public static final String MODNAME = "Super Tech Tweaks";
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
     @Mod.Instance(MODID)
     public static SuperTechTweaksMod instance;
 
