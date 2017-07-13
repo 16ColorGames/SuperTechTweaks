9160221369a80c74e59689afbde9eefa92828989
oa10712 <oa10712xbox@gmail.com>
2017-07-13 18:53:19 -0500

    1.4.0 version bump

--- src/main/java/com/sixteencolorgames/supertechtweaks/SuperTechTweaksMod.java
+++ src/main/java/com/sixteencolorgames/supertechtweaks/SuperTechTweaksMod.java
@@ -7,14 +7,13 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
 
 @Mod(modid = SuperTechTweaksMod.MODID, name = SuperTechTweaksMod.MODNAME, version = SuperTechTweaksMod.VERSION, acceptedMinecraftVersions = "[1.10.2]")
 public class SuperTechTweaksMod {
 
     public static final String MODID = "supertechtweaks";
     public static final String MODNAME = "Super Tech Tweaks";
-    public static final String VERSION = "1.3.3";
+    public static final String VERSION = "1.4.0";
     @Mod.Instance(MODID)
     public static SuperTechTweaksMod instance;
 
